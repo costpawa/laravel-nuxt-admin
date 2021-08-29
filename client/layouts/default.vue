bd47b150c5f5bc61bfa2a96e2d9b1d0514862d61
costpawa <hakan_dnz_000@hotmail.com>
2021-08-29 18:59:44 +0300

    Created breadcrumbs Deleted Navigation.vue component

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="w-full">
       <Navbar />
-      <Navigation class="mt-2 my-3 ml-3" />
+      <Breadcrumbs class="mt-2 my-3 ml-3" />
       <Nuxt class="mb-3 mx-3 p-3 bg-white relative"/>
     </div>
   </div>
