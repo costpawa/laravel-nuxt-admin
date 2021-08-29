0b009a0db248a349e10c01fffcdcc33f05643f84
costpawa <hakan_dnz_000@hotmail.com>
2021-08-29 21:01:52 +0300

    Add animxyz plugin, update navbar

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -2,17 +2,17 @@
   <div class="h-14 w-full border-b-1 pl-3 pr-5 flex justify-between bg-white border-trueGray-300">
     <div class="flex items-center">
       <div class="navbar-links" >
-        <md-icon @click="openSearch()">search</md-icon>
-        <!-- <md-field>
+        <!-- <md-icon @click="openSearch()">search</md-icon>
+        <md-field>
           <md-input v-model="search"></md-input>
         </md-field> -->
       </div>
     </div>
     <div class="flex justify-end items-center">
-      <NuxtLink to="/" class="navbar-links">
+      <NuxtLink to="/" class="navbar-links" title="Notifications">
         <md-icon>notifications</md-icon>
       </NuxtLink>
-      <div class="navbar-links">
+      <div class="navbar-links" @click="profileToggle = !profileToggle">
         <div class="flex justify-between items-center">
           <div>
             <img class="rounded-full w-8 h-8" src="~assets/img/profile.jpg" alt="profile">
@@ -23,12 +23,14 @@
           </div>
         </div>
       </div>
-      <NuxtLink to="/" class="navbar-links ml-0">
-        <md-icon>settings</md-icon>
-        <div class="navbar-link-descriptions">
-          <div class="absolute w-0 h-0 border-l-6 border-r-6 border-b-6 border-sideBar-900 opacity-75" style="border-left-color:transparent; border-right-color:transparent;margin-left: 6px"></div>
-          <div class="bg-sideBar-900 opacity-75 text-white px-4 pb-2 pt-1 rounded-full" style="margin-top: 6px; margin-left: -50px;">Settings</div>
+      <!-- <XyzTransition xyz="fade-100% origin-top short-100% ease-in-out-back">
+        <div v-if="profileToggle">
+          zaaa
         </div>
+      </XyzTransition> -->
+
+      <NuxtLink to="/" class="navbar-links ml-0" title="Settings">
+        <md-icon>settings</md-icon>
       </NuxtLink>
     </div>
   </div>
@@ -38,17 +40,15 @@
   export default {
     data() {
       return {
-        search: 'Search...'
-      }
-    },
-    methods: {
-      openSearch() {
-
+        profileToggle: false
       }
     }
   }
 </script>
 
 <style scoped>
-  md-icon {transition: background-color .15s !important;}
+  .md-icon {transition: color .15s !important;}
+  .navbar-links:hover .md-icon {
+    color: #1E1E29 !important;
+  }
 </style>
---
+++ client/plugins/animxyz.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import VueAnimXyz from '@animxyz/vue3'
+import '@animxyz/core' // Import css here if you haven't elsewhere
+
+Vue.use(VueAnimXyz)
